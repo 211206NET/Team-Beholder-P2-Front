6a23aef203e16e2a330d3f3e6a6ddeaf25646575
MattCWiese <Matthew.Wiese@Revature.net>
2022-02-10 15:09:48 -0800

    started heal

--- GameDD/Dungeons and Danger/Assets/Scripts/HealSelf.cs
+++ GameDD/Dungeons and Danger/Assets/Scripts/HealSelf.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class HealSelf : MonoBehaviour
 {
 
-    public int healtype = 1;
+    public int healtype = 1; //1 = cleric heal, 2 = paladin heal
+    private int _heal = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //if(){}
     }
 
     // Update is called once per frame
